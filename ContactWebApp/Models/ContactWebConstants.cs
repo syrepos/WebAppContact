b6d04714dbb250b873c78a7eec738744988d0fcf
Benrhouma <syrine.benrhouma@sap.com>
2019-12-19 21:38:40 +0100

    Application's host

---
+++ ContactWebApp/Models/ContactWebConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactWebApp.Models
+{
+    public class ContactWebConstants
+    {
+        public const int MAX_STATE_NAME_LENGTH = 50;
+    }
+}
---
+++ ContactWebApp/Models/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactWebApp.Models
+{
+    public class State
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Abreviation { get; set; }
+    }
+}
