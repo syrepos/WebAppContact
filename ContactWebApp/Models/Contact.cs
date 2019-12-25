8420f83c1723a937af2785b362a56bdfe03361d9
Syrinebr <benrhoumasyrine10@gmail.com>
2019-12-25 15:27:10 +0100

    Updates regarding the contact create page

--- ContactWebApp/Models/Contact.cs
+++ ContactWebApp/Models/Contact.cs
@@ -12,39 +12,49 @@ namespace ContactWebApp.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "First Name is required!")]
         [StringLength(ContactWebConstants.MAX_FIRST_NAME_LENGTH)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last Name is required!")]
         [StringLength(ContactWebConstants.MAX_LAST_NAME_LENGTH)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required(ErrorMessage ="Email is required")]
         [EmailAddress(ErrorMessage ="Invalid Email Adress")]
+        [Display(Name = "Email adress")]
         public string Email { get; set; }
 
         [Required(ErrorMessage ="Phone is required")]
         [Phone(ErrorMessage ="Invalid Phone number")]
         [StringLength(ContactWebConstants.MAX_PHONE_LENGTH)]
+        [Display(Name = "Mobile Phone")]
         public string PhonePrimary { get; set; }
 
         [Phone(ErrorMessage = "Invalid Phone number")]
         [StringLength(ContactWebConstants.MAX_PHONE_LENGTH)]
+        [Display(Name = "Home and office phone")]
         public string PhoneSecondary { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime Birthday { get; set; }
+        
         [StringLength(ContactWebConstants.MAX_STREETADRESS_LENGTH)]
+        [Display(Name = "Street adress line 1")]
         public string StreetAdress1 { get; set; }
+        
         [StringLength(ContactWebConstants.MAX_STREETADRESS_LENGTH)]
+        [Display(Name = "Street adress line 2")]
         public string StreetAdress2 { get; set; }
 
         [Required(ErrorMessage ="City is required")]
         [StringLength(ContactWebConstants.MAX_CITY_LENGTH)]
         public string City { get; set; }
 
-        [Required(ErrorMessage ="State is required")]
-        public int StateId { get; set; }
 
+        [Required(ErrorMessage ="State is required")]
+        [Display(Name = "State")]
+        public int StateId { get; set; }
         public virtual State State { get; set; }
 
         [Required(ErrorMessage ="Zip code is required")]
